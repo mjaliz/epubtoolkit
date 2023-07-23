bfcc2710972998207fb53ee8f5bbcbf0609fa28e
MohammadJavad14 <mrph14@gmail.com>
2023-07-23 06:40:58 +0330

    select form added

--- client/src/screens/ExtractSentenceScreen.jsx
+++ client/src/screens/ExtractSentenceScreen.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import "./extractSentence.css";
 
 const ExtractSentenceScreen = () => {
+  const handleSelect = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <div className="h-100 d-flex align-items-center justify-content-center">
       <form>
         <div id="dropZone" className="drop-zone">
           <div className="form-group">
-            <label for="fileInput">Select file:</label>
-            <input type="file" id="fileInput" className="form-control-file" />
+            <label htmlFor="fileInput">Select file:</label>
+            <input
+              type="file"
+              id="fileInput"
+              className="form-control-file"
+              onChange={handleSelect}
+            />
           </div>
           <span className="text">Drop files here or click to select files</span>
         </div>
